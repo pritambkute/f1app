0e9998c1ec546a152138c9f0abb88e2c1c3a3690
Pritam Kute <coolpritambmt@gmail.com>
2021-08-04 22:57:15 +0530

    Done boiler plate code cleanup. Added header and footer component.

---
+++ src/component/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="footer__container">
+      <span className="footer__text">&#169; Mobiquity 2021</span>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/component/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Header.css'
+
+function Header() {
+  return (
+    <div className="header__container">
+      <img className="header__image" src="https://logodownload.org/wp-content/uploads/2016/11/formula-1-logo-0.png"/>
+    </div>
+  );
+}
+
+export default Header;
